2c90dd5ad097160e4caf064a4c9897d883ce48b8
bbtjym <750225883@qq.com>
2017-12-08 23:54:14 +0800

    helloTest

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World Me',
+    motto: 'Hello World Hellllllo',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
